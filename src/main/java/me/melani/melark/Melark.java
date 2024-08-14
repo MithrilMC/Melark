bb862a719ddfc3ca6604a629a598efc221689017
Melani <coraline2719@icloud.com>
2024-08-14 13:15:35 +0200

    GitHub actions + startup time

--- src/main/java/me/melani/melark/Melark.java
+++ src/main/java/me/melani/melark/Melark.java
@@ -6,12 +6,19 @@ public final class Melark extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        long startTime = System.currentTimeMillis();
 
+        // Plugin startup logic
+        long enableTime = System.currentTimeMillis() - startTime;
+        getLogger().info("Melark plugin has been enabled! (Took " + enableTime + "ms)");
     }
 
     @Override
     public void onDisable() {
+        long disableStartTime = System.currentTimeMillis();
+
         // Plugin shutdown logic
+        long disableTime = System.currentTimeMillis() - disableStartTime;
+        getLogger().info("Melark plugin has been disabled! (Took " + disableTime + "ms)");
     }
 }
